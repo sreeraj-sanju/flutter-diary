d5384a9a90122f41c42b528e6915d5661839042c
sreeraj <sreerajs728@gmail.com>
2023-03-11 15:22:22 +0530

    launcher icon package installed and icon added

--- lib/Login.dart
+++ lib/Login.dart
@@ -10,28 +10,28 @@ class Login extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage("assets/images/img.jpg"), fit: BoxFit.cover)),
+                image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover)),
         child: Center(
           child: Container(
             decoration: BoxDecoration(boxShadow: [
               BoxShadow(
                 blurRadius: 24,
-                spreadRadius: 16,
-                color: Colors.black12.withOpacity(.5),
+                spreadRadius: 10,
+                color: Color.fromARGB(31, 250, 249, 249).withOpacity(.1),
               )
             ]),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                 child: Container(
-                  width: 300,
+                  width: 250,
                   height: 500,
                   decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(.3),
+                      color: Colors.white.withOpacity(.4),
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(
-                          width: 1.5, color: Colors.white30.withOpacity(.2))),
+                          width: 1.5, color: Colors.white.withOpacity(.2))),
                           child: Center(child: const Text('data')),
                 ),
               ),
